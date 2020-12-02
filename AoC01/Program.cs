409cb31bcf5b63f89b695f5a0ce0e623cfce6c18
Thomas Hilbert <me@thomas-hilbert.name>
2020-12-03 00:13:20 +0100

    Solved 1.1 and 1.2

--- AoC01/Program.cs
+++ AoC01/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using System.Linq;
+using static System.Console;
+using static System.IO.File;
 
-namespace AoC01
-{
-	class Program
+int[] inputs = ReadAllLines("input.txt").Select(line => int.Parse(line)).ToArray();
+
+Write("Part 1: ");
+for (int i = 0; i < inputs.Length; ++i)
+	for (int j = i + 1; j < inputs.Length; ++j)
 	{
-		static void Main(string[] args)
+		if (inputs[i] + inputs[j] == 2020)
 		{
-			Console.WriteLine("Hello World!");
+			WriteLine(inputs[i] * inputs[j]);
 		}
 	}
-}
+
+Write("Part 2: ");
+for (int i = 0; i < inputs.Length; ++i)
+	for (int j = i + 1; j < inputs.Length; ++j)
+		for (int k = j + 1; k < inputs.Length; ++k)
+		{
+			if (inputs[i] + inputs[j] + inputs[k] == 2020)
+			{
+				WriteLine(inputs[i] * inputs[j] * inputs[k]);
+			}
+		}
